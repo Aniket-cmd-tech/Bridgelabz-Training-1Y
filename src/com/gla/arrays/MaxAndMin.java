9c95cbcf44d34e6b3d39419914fc8deb4dccdce8
Aniket <aniketgangwar09@gmail.com>
2026-02-03 11:54:19 +0530

    all codes added

---
+++ src/com/gla/arrays/MaxAndMin.java
@@ -0,0 +1,16 @@
+package com.gla.arrays;
+
+import java.util.*;
+class MaxValue1D {
+    static void main(String[] args) {
+        int max=0;
+        int []arr={3,5,7,8,1};
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("MAX element of the given array:"+max);
+    }
+}
+
